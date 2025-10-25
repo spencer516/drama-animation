ca52376141d2586a3ae5a2172ae5f96ca221100a
jesuitdrama <drama@jhssac.org>
2025-10-25 15:09:40 -0700

    boilerplate

--- src/projects/2-blue-and-chaos/scene.tsx
+++ src/projects/2-blue-and-chaos/scene.tsx
@@ -1,8 +1,17 @@
-import { makeScene2D } from "@motion-canvas/2d";
+import { Line, makeScene2D, Rect } from "@motion-canvas/2d";
 import { createFilledGrid, setupLEDScene } from "@/lib/LEDSystem";
-import { Color, sequence, waitFor } from "@motion-canvas/core";
-import { sequenceRows } from "@/lib/wall-coordinate-system";
-import { GRID_BLUE, LED_BLUE } from "@/lib/design-system";
+import { Color, createRefArray, sequence, waitFor } from "@motion-canvas/core";
+import {
+  positionToCoordinates,
+  positionToRect,
+  sequenceRows,
+} from "@/lib/wall-coordinate-system";
+import {
+  GRID_BLUE,
+  GRID_LINE_WIDTH,
+  GRID_WHITE,
+  LED_BLUE,
+} from "@/lib/design-system";
 
 export default makeScene2D(function* (view) {
   const { ledSystem, screen } = setupLEDScene(view);
@@ -14,5 +23,31 @@ export default makeScene2D(function* (view) {
     gridColor: GRID_BLUE,
   });
 
-  yield* waitFor(10);
+  const randomLines = createRefArray<Line>();
+  const randomBoxes = createRefArray<Rect>();
+
+  const rect = positionToRect([4, 3], 2, 1);
+
+  screen().add(
+    <>
+      <Line
+        ref={randomLines}
+        points={[positionToCoordinates([2, 0]), positionToCoordinates([2, 1])]}
+        lineWidth={GRID_LINE_WIDTH}
+        stroke={GRID_WHITE}
+      />
+      <Rect
+        ref={randomBoxes}
+        x={rect.x}
+        y={rect.y}
+        width={rect.width}
+        height={rect.height}
+        stroke={GRID_WHITE}
+        fill={GRID_WHITE}
+        lineWidth={GRID_LINE_WIDTH}
+      />
+    </>
+  );
+
+  yield* waitFor(15);
 });
