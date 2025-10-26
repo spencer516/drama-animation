00cf24e05b1d2e3b50b1d0c2db35fcb38d3f2e7a
jesuitdrama <drama@jhssac.org>
2025-10-26 12:48:29 -0700

    remove old transition

--- src/projects/3-transition-TL-to-BR/project.ts
+++
@@ -1,8 +0,0 @@
-import { makeProject } from "@motion-canvas/core";
-import scene from "./scene?scene";
-
-export default makeProject({
-  name: "3-transition-TL-to-BR",
-  scenes: [scene],
-  experimentalFeatures: true,
-});
