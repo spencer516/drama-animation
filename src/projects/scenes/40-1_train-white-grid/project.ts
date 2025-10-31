5090412abfdeb3199c0802e7b2da6abac3cf6cc7
jesuitdrama <drama@jhssac.org>
2025-10-30 20:31:56 -0700

    fix judy fade in

--- src/projects/scenes/40-1_train-white-grid/project.ts
+++ src/projects/scenes/40-1_train-white-grid/project.ts
@@ -2,7 +2,7 @@ import { makeProject } from "@motion-canvas/core";
 import scene from "./scene?scene";
 
 export default makeProject({
-  name: "40-1_train-fade-out",
+  name: "40-1_train-white-grid",
   scenes: [scene],
   experimentalFeatures: true,
 });
--- src/projects/scenes/41-1_judy-fade-in/scene.tsx
+++ src/projects/scenes/41-1_judy-fade-in/scene.tsx
@@ -1,12 +1,22 @@
 import { makeScene2D } from "@motion-canvas/2d";
 import { createFilledGrid, setupLEDScene } from "@/lib/LEDSystem";
 import { waitFor } from "@motion-canvas/core";
-import { GRID_PURPLE, LED_PURPLE } from "@/lib/design-system";
+import {
+  GRID_PURPLE,
+  GRID_WHITE,
+  LED_ON,
+  LED_PURPLE,
+} from "@/lib/design-system";
 
 export default makeScene2D(function* (view) {
   const { ledSystem, screen } = setupLEDScene(view);
 
-  const { fillAnimated } = createFilledGrid(ledSystem, screen);
+  const { fillAnimated, fill } = createFilledGrid(ledSystem, screen);
+
+  fill({
+    ledColor: LED_ON,
+    gridColor: GRID_WHITE,
+  });
 
   yield* fillAnimated(
     {
