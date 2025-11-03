cae668c77ee3b0c84e983bd0abc3fc7cede54816
Spencer P <spencer516@gmail.com>
2025-11-03 18:07:59 -0500

    add 37-1 other train station movement

---
+++ src/projects/scenes/37-1_train-station-movement/project.ts
@@ -0,0 +1,8 @@
+import { makeProject } from "@motion-canvas/core";
+import scene from "./scene?scene";
+
+export default makeProject({
+  name: "37-1_train-station-movement",
+  scenes: [scene],
+  experimentalFeatures: true,
+});
